33ac9d13131fd8893d0e5ced8c1f5861744babd8
Agnar <agnar-97@hotmail.com>
2019-09-30 08:34:34 +0000

    First commit

---
+++ src/main/java/is/hi/hbv501G/mundus/Mundus/MundusApplication.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501G.mundus.Mundus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MundusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MundusApplication.class, args);
+	}
+
+}
