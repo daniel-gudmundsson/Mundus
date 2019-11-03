157c45bad311c8a1c8a681e33c5b9edf63818af1
zeppelin-43 <dantgud@gmail.com>
2019-11-03 14:35:49 +0100

    Update ProfileManagementController.java

--- src/main/java/is/hi/hbv501G/mundus/Mundus/Controllers/ProfileManagementController.java
+++ src/main/java/is/hi/hbv501G/mundus/Mundus/Controllers/ProfileManagementController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ProfileManagementController {
-    // We are not sure what we are going to do with this one ¯\_(ツ)_/¯ 
+    // We are not sure what we are going to do with this one ¯\_(ツ)_/¯
     @RequestMapping("/profile")
     public String home(){
         return "profile";
